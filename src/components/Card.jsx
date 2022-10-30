ae7f0a820e4bf1d8dea567679e4f23428953fdf6
RamunnoAJ <ramunnoagustin@gmail.com>
2022-10-30 15:38:10 -0300

    fix: gap between items

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Card({ title, subtitle, image, text, id }) {
   return (
     <div>
-      <article className=' flex flex-col px-6 py-10 gap-8  bg-primary-dark max-w-sm rounded-lg text-white'>
+      <article className=' flex flex-col px-6 py-10 gap-8  bg-primary-dark max-w-[26rem] rounded-lg text-white'>
         <div className='flex justify-start items-center gap-8'>
-          <img src={image} alt='' className='w-16 h-16 rounded-full' />
+          <img src={image} alt='' className='w-14 h-14 rounded-full' />
           <div>
             <p className=' font-bold text-lg tracking-wide'>{title}</p>
             <p className='text-primary-light font-medium'>{subtitle}</p>
