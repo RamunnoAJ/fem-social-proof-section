7757ac9feb352374255c7a0d092f6b1d9dd0ed16
RamunnoAJ <ramunnoagustin@gmail.com>
2022-10-31 01:58:26 -0300

    fix: HTML validation reports

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,9 +5,13 @@ function Card({ title, subtitle, image, text, id }) {
     <div>
       <article className=' flex flex-col px-6 py-10 gap-8  bg-primary-dark max-w-[26rem] rounded-lg text-white'>
         <div className='flex justify-start items-center gap-8'>
-          <img src={image} alt='' className='w-14 h-14 rounded-full' />
+          <img
+            src={image}
+            alt={`image of ${title}`}
+            className='w-14 h-14 rounded-full'
+          />
           <div>
-            <p className=' font-bold text-lg tracking-wide'>{title}</p>
+            <h3 className=' font-bold text-lg tracking-wide'>{title}</h3>
             <p className='text-primary-light font-medium'>{subtitle}</p>
           </div>
         </div>
--- src/components/icons/Star.jsx
+++ src/components/icons/Star.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import starIcon from '../../assets/icon-star.svg'
 
 export default function Star() {
-  return <img src={starIcon} aria-hidden='true' />
+  return <img src={starIcon} alt='Icon of a star' aria-hidden='true' />
 }
